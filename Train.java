88c47400c892022fde81beb2bf609da99673c4e1
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-07 23:02:38 +0300

    Add files via upload

---
+++ Train.java
@@ -0,0 +1,26 @@
+package mmn11;
+import java.util.Scanner;
+
+public class Train 
+{
+	public static void main(String[] args)
+	{
+		double x1,x2,deltaX;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("please enter the speed of train 1:");
+		int v1=scan.nextInt();
+		System.out.println("please enter the stime of train 1:");
+		double t1=scan.nextDouble();
+		t1=t1/60;
+		System.out.println("please enter the speed of train 2:");
+		int v2=scan.nextInt();
+		System.out.println("please enter the stime of train 2:");
+		double t2=scan.nextDouble();
+		t2=t2/60;
+		x1=v1*t1;
+		x2=v2*t2;
+		deltaX=Math.abs(x1-x2);
+		System.out.println(deltaX);
+		
+	}
+}
