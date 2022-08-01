0fb83ded1358056184fe92041d93426d85ea4320
eyal <eyal5000192@gmail.com>
2022-08-01 12:25:24 +0300

    1

--- Temperature.java
+++ Temperature.java
@@ -4,6 +4,8 @@ public class Temperature
 {
     public static void main(String[] args)
     {
-        
+        Scanner scan=new Scanner(System.in);
+        System.out.println("please Enter the temperature:");
+        double t1=scan.nextDouble();
     }
 }
