fa702c0b76efc26d498eec14e206e9d13dacb3aa
eyal <eyal5000192@gmail.com>
2022-08-02 22:58:42 +0300

    if1

--- Temperature.java
+++ Temperature.java
@@ -4,13 +4,21 @@ public class Temperature
 {
     public static void main(String[] args)
     {
+        double c=0,k=0;
         Scanner scan=new Scanner(System.in);
         System.out.println("please Enter the temperature:");
         double t1=scan.nextDouble();
         System.out.println("please enter the Unit of measure:");
         String unit=scan.next();
-        if(unit.equals("f")||unit.equals("F"));
-            System.out.println(t1);
-        System.out.println(unit);
+        if(unit.equals("f"))
+        {
+           c=(5*(t1-32))/9;
+           k=c+273.15;
+           System.out.println(c+" c");
+           System.out.println(t1+" f");
+           System.out.println(k+" k");
+        }
+        else
+           System.out.println("Not yet written");
     }
 }
