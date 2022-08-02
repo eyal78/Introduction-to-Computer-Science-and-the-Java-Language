6a390f7683c54d7c578d0a7a79a37b007f4d2516
eyal <eyal5000192@gmail.com>
2022-08-02 18:39:49 +0300

    1

--- Temperature.java
+++ Temperature.java
@@ -7,5 +7,9 @@ public class Temperature
         Scanner scan=new Scanner(System.in);
         System.out.println("please Enter the temperature:");
         double t1=scan.nextDouble();
+        System.out.println("please enter the Unit of measure:");
+        String unit=scan.next();
+        System.out.println(t1);
+        System.out.println(unit);
     }
 }
