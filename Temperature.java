861be2f057557176d94257d721d281fc543c1a18
eyal <eyal5000192@gmail.com>
2022-08-02 22:13:08 +0300

    1

--- Temperature.java
+++ Temperature.java
@@ -9,7 +9,8 @@ public class Temperature
         double t1=scan.nextDouble();
         System.out.println("please enter the Unit of measure:");
         String unit=scan.next();
-        System.out.println(t1);
+        if(unit.equals("f")||unit.equals("F"));
+            System.out.println(t1);
         System.out.println(unit);
     }
 }
