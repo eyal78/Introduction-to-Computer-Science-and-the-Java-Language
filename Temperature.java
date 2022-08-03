cc217d1fe55eec4af242615da055dfaa94569190
eyal <eyal5000192@gmail.com>
2022-08-03 22:17:43 +0300

    if2

--- Temperature.java
+++ Temperature.java
@@ -4,7 +4,7 @@ public class Temperature
 {
     public static void main(String[] args)
     {
-        double c=0,k=0;
+        double c=0,k=0,f=0;
         Scanner scan=new Scanner(System.in);
         System.out.println("please Enter the temperature:");
         double t1=scan.nextDouble();
@@ -14,11 +14,26 @@ public class Temperature
         {
            c=(5*(t1-32))/9;
            k=c+273.15;
-           System.out.println(c+" c");
-           System.out.println(t1+" f");
-           System.out.println(k+" k");
+           f=t1;
         }
+        else 
+        if(unit.equals("c")) 
+        {
+           k=c+273.15;
+           f=(9*(k-273.15))/5+32;
+           c=t1;
+        } 
+        else 
+        if(unit.equals("k"))
+        {
+           k=t1;
+           f=(9*(k-273.15))/5+32;
+           c=5*(f-32)/9;
+        } 
         else
-           System.out.println("Not yet written");
+            System.out.println("There is an error with the temperature unit");
+        System.out.println(c+" c");
+        System.out.println(t1+" f");
+        System.out.println(k+" k");
     }
 }
